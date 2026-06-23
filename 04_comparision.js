63ebf439447a06c44febb215ad5d354414c3e7eb
Nivedita <niveditaraj884@gmail.com>
2026-06-23 08:53:05 +0000

    comparision

---
+++ 04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0 );
+console.log( null == 0);
+console.log( null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+ // avoid this type of conversion(line 11 to 17)
+
+
+// === (it check the value stricly)
+
+console.log( "2" === 2);
+
+
